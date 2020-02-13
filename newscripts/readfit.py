b30a273209f3d51c07ce4bb3c0823d951ccdce87
Luca Comba <comb6457@stthomas.edu>
2020-02-13 00:27:41 -0600

    deliting displaying and adding comments

--- newscripts/readfit.py
+++ newscripts/readfit.py
@@ -1,18 +1,42 @@
 #########################################
 # Luca Comba, for UST
 #########################################
+
 import os
 import numpy as np
+import sys
+import math
+import numpy
 from PIL import Image
 from astropy.io import fits
+from matplotlib.colors import *
+import matplotlib.pyplot as plt
+from astropy.visualization import *
+from astropy.utils.data import get_pkg_data_filename
 
-"""
-CREATING FITS IMAGE OBJECT
-Methods : __init__ , close, printInfo, printData
-setHeader, 
-""" 
+'''USEFUL FUNCTIONS'''
+# Fuction to visualize the array of pixels
+def exportingArrayAsTxt(pixelarray):
+	pixelarray = np.array2string(pixelarray)
+	file = open("pixelarray.txt", "w+")
+
+	for array in pixels:
+		file.write(array)
+
+	file.close()
+
+def imageStack():
+	return True
+
+'''CREATING FITS IMAGE OBJECT
+Methods : __init__ , close ,
+  printInfo, printDate , setHeader , getHeaderKeys ,
+  getShape , getNameType , getPixelArray , getPixelByIndex,
+  printStats, stretchMinMax.
+''' 
 class FitsData:
-	#General
+	############
+	# CONTRUCTOR
 	def __init__(self, filename):
 		self.img = fits.open("data/"+filename)
 		self.hdr = self.img[0].header
@@ -20,7 +44,8 @@ class FitsData:
 	
 	def close(self):
 		self.img.close()
-	#Meta
+	############
+	# META
 	def printInfo(self):
 		print(self.img.info())
 
@@ -41,33 +66,120 @@ class FitsData:
 		self.hdr = self.img[0].header
 		return self.hdr[key]
 
-	#WORKING WITH IMG
+	def printStats(self, Min, Max, Mean, StDev):
+		if (Min == True):
+			print('Min:', np.min(self.data[0:]))
+		elif (Max == True):
+			print('Max:', np.max(self.data[0:]))
+		elif (Mean == true):
+			print('Mean:', np.mean(self.data[0:]))
+		elif (StDev == True):
+			print('Stdev:', np.std(self.data[0:]))
+		else:
+			print('Min:', np.min(self.data[0:]))
+			print('Max:', np.max(self.data[0:]))
+			print('Mean:', np.mean(self.data[0:]))
+			print('Stdev:', np.std(self.data[0:]))
+	##################
+	# WORKING WITH IMG
 	def getShape(self):
 		base = self.getShape()[0]
 		height = self.getShape()[1]
 		return self.data.shape
-	def getName():
+	def getNameType():
 		return self.data.dtype.name
 	def getPixelArray(self):
 		return self.data[0:]
+	# TABLE DATA
+	def getPixelByIndex(self, index):
+		tbdata = self.img[0].data
+		return tbdata[index]
+	# IMAGE VISUALIZATION
+	def stretchMinMax(self, typeStretch = LinearStretch(), show = True):
+		pixels = self.data[0:]
+		stretch = typeStretch
+		norm = ImageNormalize(pixels, interval=MinMaxInterval(), stretch=stretch)
+		if(show==True):
+			fig = plt.figure()
+			ax = fig.add_subplot(1, 1, 1)
+			im = ax.imshow(pixels, origin='lower', norm=norm, cmap='gray')
+			fig.colorbar(im)
+			plt.show()
+		else:
+			return norm
+
+def arrayStretchShow(pixels, typeStretch = LinearStretch(), show = True):
+	stretch = typeStretch
+	norm = ImageNormalize(pixels, interval=MinMaxInterval(), stretch=stretch)
+	if(show==True):
+		fig = plt.figure()
+		ax = fig.add_subplot(1, 1, 1)
+		im = ax.imshow(pixels, origin='lower', norm=norm, cmap='gray')
+		fig.colorbar(im)
+		plt.show()
+	else:
+		return norm
+
+def arrayStretch(pixels, typeStretch):
+	makeIt = typeStretch
+	makeIt(values = pixels, clip=False, out = pixels)
+	return pixels
+
+### MAIN RUNNING PROGRAM
 
 def Main():
+	forCasting = np.float_()
 	path = os.getcwd()
 	for filename in os.listdir(path+"/data"):
 		if filename.endswith(".fit") or filename.endswith(".fits") or filename.endswith(".fts"):
-			# read fits as Astropy do :
-			"""
-			hdul = fits.open("data/"+filename)
-			hdul.close()
-			"""
-
+			# want to print all the data ?
+			np.set_printoptions(threshold=sys.maxsize)
+			# Creating the object
 			hudl = FitsData(filename)
-			print(hudl.getPixelArray())
-			hudl.close()
+			pixels = hudl.getPixelArray()
+			### Stretching on object image			
+			hudl.stretchMinMax(SqrtStretch(), show = False)
 
+			### SAVING
+			forCasting = np.float_()
+			if ('Red' in filename or 'red' in filename):
+				image_r = np.array(pixels, forCasting)
+				## stretching on array
+				image_r = arrayStretch(image_r, SqrtStretch())
+				image_r = np.array(image_r, forCasting)
+			elif ('Green' in filename or 'green' in filename):
+				image_g = np.array(pixels, forCasting)
+				image_g = arrayStretch(image_g, SqrtStretch())
+				image_g = np.array(image_g, forCasting)
+
+			elif ('Blue' in filename or 'blue' in filename):
+				image_b = np.array(pixels, forCasting)
+				image_b = arrayStretch(image_b, SqrtStretch())
+				image_b = np.array(image_b, forCasting)
+			
+			### Closing
+			hudl.close()
+	
+	##### EXPORTING FINAL IMAGE
+
+	#registration, pyraf,zscal(minmax), blending,
+	image = make_lupton_rgb(image_g,image_r,image_b) #, filename = 'final.jpeg' 
+	#norm = ImageNormalize(image, interval=MinMaxInterval(),stretch=PowerStretch(0.5))
+	plt.imshow(image, origin='lower')
+	plt.show()
+	
+###### RUNNING PROGRAM
 Main()
 
 
+'''
+Remeber HOW TO BASIC : 
+
+- PLOT AN IMAGE :
+	plt.imshow(image_r, origin='lower')
+	plt.show()
 
 
 
+
+'''
