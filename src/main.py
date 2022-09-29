b618be131f25abfdc7ebef096319a3e4e0ba2dfd
lukfd <luca.comba98@gmail.com>
2022-09-28 22:35:46 -0500

    Updated README with new todo, added args.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
+import argparse
 # OUR LIBS
-import lib.showImage as show
-import lib.fitsInfo as fit
 import lib.imageRegistration as imageRegistration
 import lib.imageProcessing as processor
 import lib.convertUtils as converter
@@ -9,42 +8,54 @@ import lib.convertUtils as converter
 # have been background-subtracted, if necessary linearized,
 # flat-fielded, and scaled appropriately.
 
+# command line args
+# https://docs.python.org/3/library/argparse.html#argparse.ArgumentParser.add_argument
+parser = argparse.ArgumentParser(description='Process fits images and produce a color image')
+parser.add_argument("-r", "--red", default="../data/M66-Color/M66-S001-Red-R001-Red.fts",
+                    help="Relative path to red image", dest="ndarrayR")
+parser.add_argument("-g", "--green", default="../data/M66-Color/M66-S001-Green-R001-Green.fts",
+                    help="Relative path to green image", dest="ndarrayG")
+parser.add_argument("-b", "--blue", default="../data/M66-Color/M66-S002-Blue-R002-Blue.fts",
+                    help="Path to blue image", dest="ndarrayB")
+# NOT IMPLEMENTED
+parser.add_argument("-s", "--stretch", nargs=1, default=["linear,70,0.3"],
+                    help="Comma separated list of stretch and its parameter. Example: linear,70,0.3", 
+                    dest="inputStretch")
+
+args = parser.parse_args()
+ndarrayR = args.ndarrayR
+ndarrayG = args.ndarrayG
+ndarrayB = args.ndarrayB
+
 # Convert images to ndarray
 print("Converting images to ndarray")
-ndarrayR = converter.convertImageToNdarray("../data/M66-Color/M66-S001-Red-R001-Red.fts")
-ndarrayG = converter.convertImageToNdarray("../data/M66-Color/M66-S001-Green-R001-Green.fts")
-ndarrayB = converter.convertImageToNdarray("../data/M66-Color/M66-S002-Blue-R002-Blue.fts")
+ndarrayR = converter.convertImageToNdarray(ndarrayR)
+ndarrayG = converter.convertImageToNdarray(ndarrayG)
+ndarrayB = converter.convertImageToNdarray(ndarrayB)
 
 # Stretch arrays
-# print("Stretching images")
+print("Stretching images")
 ndarrayR = processor.applyStrech(ndarrayR, slope=70, intercept=0.3)
 ndarrayG = processor.applyStrech(ndarrayG, slope=50, intercept=0.2)
 ndarrayB = processor.applyStrech(ndarrayB, slope=100, intercept=0.3)
 
-print("NDARRA RED")
-print(ndarrayR)
-print("")
+# print("NDARRA RED")
+# print(ndarrayR)
+# print("")
 
-print("NDARRA GREEN")
-print(ndarrayG)
-print("")
+# print("NDARRA GREEN")
+# print(ndarrayG)
+# print("")
 
-print("NDARRA BLUE")
-print(ndarrayB)
-print("")
-
-# print("Plotting histogram")
-# show.plotArrayHistogram(ndarrayR)
-
-#gbImages = [ndarrayG, ndarrayB]
+# print("NDARRA BLUE")
+# print(ndarrayB)
+# print("")
 
 # Shift images
 print("Shifting images")
-# shifitedImages = [ndarrayR]
-# for image in gbImages:
-#     shifitedImages.append(imageRegistration.getShiftedImage(ndarrayR, image))
 ndarrayG = imageRegistration.getShiftedImage(ndarrayR, ndarrayG)
 ndarrayB = imageRegistration.getShiftedImage(ndarrayR, ndarrayB)
 
 # Create color picture
+print("Processing to color image")
 processor.plotColoredImage(ndarrayR, ndarrayG, ndarrayB)
